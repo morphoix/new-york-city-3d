36dfee44fef39137de52cca4d30b8432cb0dc19b
Lera <morphoix@gmail.com>
2025-02-11 11:36:43 +0100

    chore: change scripts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   distDir: "dist",
-  output: "standalone",
 };
 
 export default nextConfig;
