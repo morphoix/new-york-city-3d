ebbfadea84239569359bb8087090b20279fdf763
Lera <morphoix@gmail.com>
2025-02-24 18:47:25 +0100

    fix: loop animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
 import { Map } from "@/components/Map";
 
 export default function Home() {
-  return <Map />;
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKey((prevKey) => prevKey + 1); // Change key to force re-render
+    }, 40000); // 40 seconds
+
+    return () => clearInterval(interval); // Cleanup
+  }, []);
+
+  return <Map key={key} />;
 }
